565b88bd170d9ed8c1229273608c2764ff152b7b
jacio1 <slavarew3273@gmail.com>
2025-11-12 10:51:53 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="px-2.5 max-w-[1220px] mx-auto">
       <Header />
-      <Title/>
+      <div className="flex justify-between gap-10 m">
+        <Title/>
+        <Title/>
+        <Title/>
+      </div>
     </div>
   );
 }
